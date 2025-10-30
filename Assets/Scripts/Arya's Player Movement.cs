2967d1e73e67f33dd6e08d3b8ed6dfb6ab6a99af
Arya Parvaresh <aryaparvaresh@gmail.com>
2025-10-30 01:13:36 -0500

    Main Character can now walk!!!

--- Assets/Scripts/Arya's Player Movement.cs
+++ Assets/Scripts/Arya's Player Movement.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Tilemaps;
+using UnityEngine.InputSystem;
 
 public class AryasPlayerMovement : MonoBehaviour
 {
-     public float speed = 5; //how fast player moves 
-     public int facingDirection = 5; //controls direction sprite is facing
+     [SerializeField] public float moveSpeed = 5; //how fast player moves 
      public Rigidbody2D rb; //Must communicate with rigid body
-     public Animator anim;
+     private Vector2 moveInput;
+     public int facingDirection = 5; //controls direction sprite is facing
+
+     public Animator animator;
 
      public Player_Combat player_Combat;
 
@@ -17,8 +20,14 @@ public class AryasPlayerMovement : MonoBehaviour
      public float attackCooldown = 0.5f;     // how long to wait between attacks
      private float lastAttackTime;           // stores the last time an attack was made
 
+     void Start()
+     {
+          rb = GetComponent<Rigidbody2D>();
+          animator = GetComponent<Animator>();
+     }
      private void Update()
      {
+          rb.linearVelocity = moveInput * moveSpeed;
           // Attack input with cooldown check
           if (Input.GetButtonDown("Horizontal Attack") && Time.time >= lastAttackTime + attackCooldown)
           {
@@ -27,7 +36,44 @@ public class AryasPlayerMovement : MonoBehaviour
           }
      }
 
-     //Dashing 
+   
+     public void Move(InputAction.CallbackContext context)
+     {
+          animator.SetBool("IsWalking", true);
+
+          if (context.canceled)
+          {
+               animator.SetBool("IsWalking", false);
+               animator.SetFloat("LastInputX", moveInput.x);
+               animator.SetFloat("LastInputY", moveInput.y);
+
+          }
+        
+          moveInput = context.ReadValue<Vector2>();
+          animator.SetFloat("InputX", moveInput.x);
+          animator.SetFloat("InputY", moveInput.y);
+
+     }
+
+    
+
+     
+     /* private IEnumerator Dash()
+      {
+           canDash = false;
+           isDashing = true;
+           float originalGravity = rb.gravityScale;
+           rb.gravityScale = 0f;
+           rb.linearVelocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+           tr.emitting = true;
+           yield return new WaitForSeconds(dashingTime);
+           tr.emitting = false;
+           rb.gravityScale = originalGravity;
+           isDashing = false;
+           yield return new WaitForSeconds(dashingCooldown);
+           canDash = true;
+      }
+  //Dashing 
      private bool canDash = true;
      private bool isDashing;
      private float dashingPower = 24f;
@@ -35,62 +81,7 @@ public class AryasPlayerMovement : MonoBehaviour
      private float dashingCooldown = 1f;
 
      [SerializeField] private TrailRenderer tr;
-     // Update is called 50x per frame
-     void FixedUpdate()
-     {
-          float horizontal = Input.GetAxis("Horizontal");
-          float vertical = Input.GetAxis("Vertical");
-
-          // Set facing direction for attack logic
-          if (horizontal != 0)
-               facingDirection = 1;  // side (used for both left/right)
-          else if (vertical > 0)
-               facingDirection = 2;  // up
-          else if (vertical < 0)
-               facingDirection = 3;  // down
-
-          if (isDashing)
-          {
-               return;
-          }
-          if (horizontal > 0 && transform.localScale.x < 0 || horizontal < 0 && transform.localScale.x > 0)
-          {
-               Flip();
-          }
-          anim.SetFloat("horizontal", horizontal);
-          anim.SetFloat("vertical", vertical);
-
-          rb.linearVelocity = new Vector2(horizontal, vertical) * speed;
-
-          if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
-          {
-               StartCoroutine(Dash());
-          }
-     }
-
-     void Flip()
-     {
-          facingDirection *= -1;
-          transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.y);
-     }
-
-     private IEnumerator Dash()
-     {
-          canDash = false;
-          isDashing = true;
-          float originalGravity = rb.gravityScale;
-          rb.gravityScale = 0f;
-          rb.linearVelocity = new Vector2(transform.localScale.x * dashingPower, 0f);
-          tr.emitting = true;
-          yield return new WaitForSeconds(dashingTime);
-          tr.emitting = false;
-          rb.gravityScale = originalGravity;
-          isDashing = false;
-          yield return new WaitForSeconds(dashingCooldown);
-          canDash = true;
-     }
-
-
+     */
 
 }
 
