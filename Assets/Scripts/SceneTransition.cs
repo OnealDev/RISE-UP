f25feb74190f3d3328d671a3bed99cee08055c73
Cout  "O'Neal <166154583+OnealDev@users.noreply.github.com>
2025-12-03 14:06:24 -0600

    Revert "Rock final boss"
    This reverts commit 9166c4d35ffbfbdeec908a82af69f9dd0a7f7a89.

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -1,15 +1,16 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneTransition : MonoBehaviour
 {
      [Header("Name of the scene to load")]
-     public string sceneName = "Final Boss";
+     public string sceneName = "Final Boss"; // <-- set this to EXACT scene name
 
      private void OnTriggerEnter2D(Collider2D collision)
      {
           if (collision.CompareTag("Player"))
           {
-               FadeManager2.Instance.FadeOutAndLoad(sceneName);
+               SceneManager.LoadScene(sceneName);
           }
      }
 }
