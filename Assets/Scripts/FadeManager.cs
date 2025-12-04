f1ec4eae276c3b3b634645050e052fd5f2d7d432
Cout  "O'Neal <166154583+OnealDev@users.noreply.github.com>
2025-12-04 15:26:17 -0600

    update

---
+++ Assets/Scripts/FadeManager.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class FadeManager : MonoBehaviour
+{
+     public static FadeManager Instance;
+     public CanvasGroup fadePanel;
+     public float fadeSpeed = 1.2f;
+
+     private void Awake()
+     {
+          if (Instance == null)
+          {
+               Instance = this;
+               DontDestroyOnLoad(gameObject);
+          }
+          else
+          {
+               Destroy(gameObject);
+          }
+     }
+
+     private void Start()
+     {
+          // Fade in automatically when a scene loads
+          fadePanel.alpha = 1;
+          FadeIn();
+     }
+
+     public void FadeIn()
+     {
+          StartCoroutine(Fade(1, 0));
+     }
+
+     public void FadeOutAndLoad(string sceneName)
+     {
+          StartCoroutine(FadeOutLoad(sceneName));
+     }
+
+     private IEnumerator Fade(float from, float to)
+     {
+          float t = 0f;
+
+          while (t < 1)
+          {
+               t += Time.deltaTime * fadeSpeed;
+               fadePanel.alpha = Mathf.Lerp(from, to, t);
+               yield return null;
+          }
+     }
+
+     private IEnumerator FadeOutLoad(string sceneName)
+     {
+          // Fade to black
+          yield return StartCoroutine(Fade(0, 1));
+
+          // Load scene
+          SceneManager.LoadScene(sceneName);
+
+          // Fade in again
+          yield return null;
+          FadeIn();
+     }
+}
